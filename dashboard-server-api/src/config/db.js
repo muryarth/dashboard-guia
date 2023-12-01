9d434f984244c8e2b9122dbfff5261f655c3bc97
Muryarth <arthur.mury@outlook.com>
2023-12-01 00:53:54 -0300

    [dashboard-server-api] Atualizando string de conexão

--- dashboard-server-api/src/config/db.js
+++ dashboard-server-api/src/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 mongoose.set("strictQuery", false);
 
 const connectDB = async () => {
-  const dbConnectionString = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.jac7iaa.mongodb.net/?retryWrites=true&w=majority`;
+  const dbConnectionString = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority`;
   try {
     const conn = await mongoose.connect(dbConnectionString);
     console.log(`Banco conectado: ${conn.connection.host}`);

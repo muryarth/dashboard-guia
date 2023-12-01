476fc97529e468c7f314f10c909bea85240267e7
Muryarth <arthur.mury@outlook.com>
2023-12-01 01:40:22 -0300

    Revert "[dashboard-server-api] Configurando CORS"
    This reverts commit 609831f1ae6cd065dabc1cb460e74e9e8a2ae7ba.

--- dashboard-server-api/src/config/server.js
+++ dashboard-server-api/src/config/server.js
@@ -1,23 +1,10 @@
 import express from "express";
-import cors from "cors";
 // import session from "express-session";
 // import methodOverride from "method-override";
 
 const serverConfiguration = (app) => {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
-  app.use(cors());
-  
-  app.use((req, res, next) => {
-    // Configura o cabeçalho CORS para permitir qualquer origem na rede local
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    res.header("Access-Control-Allow-Credentials", "true");
-
-    // Continua para o próximo middleware
-    next();
-  });
   // app.use(methodOverride("_method"));
 
   // Express Session

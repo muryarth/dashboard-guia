3199319e669568cd49926a803c997d4155db800f
Muryarth <arthur.mury@outlook.com>
2023-12-04 15:08:25 -0300

    Revert "[dashboard-server-api] Criando configuração para o CORS"
    This reverts commit 0a3f9e6cd0374116530fc049f973530c3836e0c8.

--- dashboard-server-api/src/config/server.js
+++ dashboard-server-api/src/config/server.js
@@ -1,20 +1,23 @@
 import express from "express";
-import cors from "cors";
+// import session from "express-session";
+// import methodOverride from "method-override";
 
 const serverConfiguration = (app) => {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
-  app.use(cors);
-  app.use(() => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res.header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE");
-  });
+  // app.use(methodOverride("_method"));
 
-  next();
+  // Express Session
+  // app.use(
+  //   session({
+  //     secret: "secret",
+  //     resave: false,
+  //     saveUninitialized: true,
+  //     cookie: {
+  //       maxAge: 1000 * 60 * 60 * 24 * 7, // uma semana
+  //     },
+  //   })
+  // );
 };
 
 export default serverConfiguration;

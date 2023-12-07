d85e04b8f9db2acd2b452a5acd1b552efdeee55d
Muryarth <arthur.mury@outlook.com>
2023-12-07 16:24:04 -0300

    Testando vercel

--- dashboard-server-api/src/config/CORSConfig.js
+++ dashboard-server-api/src/config/CORSConfig.js
@@ -1,10 +1,9 @@
 import cors from "cors";
 
+const ip = process.env.DEV_IP || "localhost";
+
 const CORSConfig = (app) => {
-  // const allowedOrigins = process.env.ALLOWED_ORIGINS
-  //   ? process.env.ALLOWED_ORIGINS.split(";")
-  //   : ["http://localhost:3000"];
-  const allowedOrigins = ["*"];
+  const allowedOrigins = [`http://${ip}:3000`, `http://localhost:3000`, "*"];
 
   const corsOptions = {
     origin: function (origin, callback) {

94a5338a7964efa51e727d63821c11beb75a39ee
Muryarth <arthur.mury@outlook.com>
2023-12-09 18:45:21 -0300

    [dashboard-view-react] Retirando alguns imports desnecessários

--- dashboard-view-react/src/telas/customers/home/components/AuthorizationModal/index.jsx
+++ dashboard-view-react/src/telas/customers/home/components/AuthorizationModal/index.jsx
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from "react";
 
 //Bootstrap
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import Dropdown from "react-bootstrap/Dropdown";
